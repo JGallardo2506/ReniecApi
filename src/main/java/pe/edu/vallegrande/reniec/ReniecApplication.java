a26ff97af72eb6a8d612391826d36e727281967d
Jose Gallardo <jose.gallardo@vallegrande.edu.pe>
2024-12-16 22:49:12 -0500

    Tu mensaje de commit aquí

---
+++ src/main/java/pe/edu/vallegrande/reniec/ReniecApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.vallegrande.reniec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReniecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReniecApplication.class, args);
+	}
+
+}
